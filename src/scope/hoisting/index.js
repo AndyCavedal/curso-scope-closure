06c20552d4c4f7a9efdf0205e7883104da4c3443
Andy <hcavedal@gmail.com>
2023-02-23 13:23:39 -0300

    agregado debug y hoisting

---
+++ src/scope/hoisting/index.js
@@ -0,0 +1,10 @@
+// console.log(nameOfDog);
+// var nameOfDog = 'Elmo';
+// console.log(nameOfDog);
+
+nameOfDog();
+
+function nameOfDog() {
+    var elmo = 'elmito';
+    console.log(`El mejor perrito es ${elmo}`);
+}
